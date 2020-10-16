9524a75cfa6f38e157cea3c1d284e12b71663aa5
bernardo emilio valderrama canto <emiliocanto13@gmail.com>
2020-10-16 11:03:56 -0300

    creado pagina de inicio

--- locallibreria/catalogo/models.py
+++ locallibreria/catalogo/models.py
@@ -1,6 +1,7 @@
-from django.db import models			
-from django.urls import reverse  		#redirecciona una url de un libro al browser 
-import uuid  							#se utiliza para definir atributos clave (pk)
+from django.db import models
+from django.urls import reverse # Used to generate URLs by reversing the URL patterns
+import uuid # Required for unique book instances
+
 
 # Create your models here.
 class Genre(models.Model):
@@ -10,15 +11,13 @@ class Genre(models.Model):
 	def __str__(self):
 		return self.name
 
-
-
 class Book(models.Model):
     
 	title = models.CharField(max_length=200)
 	author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
     
-	summary = models.TextField(max_length=1000)
-	isbn = models.CharField('ISBN', max_length=13)
+	summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
+	isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
 	genre = models.ManyToManyField(Genre)
     
 	def __str__(self):
@@ -27,9 +26,6 @@ class Book(models.Model):
 	def get_absolute_url(self):
 		"""Returns the url to access a detail record for this book."""
 		return reverse('book-detail', args=[str(self.id)])
-
-
-
 
 
 class BookInstance(models.Model):
